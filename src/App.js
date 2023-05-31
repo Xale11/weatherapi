a61efbd006c637ff0aa7878b25dce38321bfb5f9
Alex Diyan <alexdiyan.ad@gmail.com>
2023-05-31 23:48:18 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import Search from './components/search';
 import Container from './components/container';
+/*update */
+
 function App() {
 
   return (
